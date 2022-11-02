725cd10e96a56971747b8a546cc6d7e790a9449f
Ezekude izuchukwu Francis <frankizuchukwu094@gmail.com>
2022-11-02 11:53:20 +0100

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -24,16 +24,3 @@ from flaskblog import routes
 
 # from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
-
-# The libraries i use for my project are...
-
-# *Html
-# *css
-# *Bootstrap
-# *Python
-# *Flask Framework
-
-# about the project, it's a Tech Blog website which a User can create account,login,logout,update his / her details or info,change profile picture and finally the user can make a post too.
-# And it's a website by which knowledge are being share, both topics,subjects and information are being share and to help new tech aspirant to acquire more knowledge 
-# and it can help a newbie in there difficulties too so they can solve there issues only by one search..those who are already good can jhelp in sharing there knowledge too....check it out
-# it's a nice website i promise and changes will be occuring at any time too
